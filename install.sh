e16ee37f15c2db95a6be6aad3049d51d974693aa
valerievandervorst <88132810+valerievandervorst@users.noreply.github.com>
2024-02-23 09:53:05 +0100

    fix config channels install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ if ! command -v conda activate &> /dev/null
         exit 1
 fi
 
-conda install -y -c conda-forge mamba
+conda install -y -c conda-forge mamba 
 
 
-mamba config --add channels bioconda && \
-mamba config --add channels anaconda && \
-mamba config --add channels r && \
-mamba config --add channels defaults && \
-mamba config --add channels conda-forge
-mamba create -n pimenta -y -c bioconda
+conda config --add channels bioconda && \
+conda config --add channels anaconda && \
+conda config --add channels r && \
+conda config --add channels defaults && \
+conda config --add channels conda-forge
+mamba create -n pimenta -y -c bioconda python=3.10
 mamba install -n pimenta -y -c bioconda cd-hit
 mamba install -n pimenta -y -c conda-forge r-base
 mamba install -n pimenta -y -c bioconda perl-cgi perl-bioperl perl-cairo perl-statistics-basic  perl-json
@@ -23,7 +23,7 @@ mamba install -n pimenta -y -c bioconda perl-cgi perl-bioperl perl-cairo perl-st
 
 mamba install -n pimenta -y blast prinseq=0.20.4 cutadapt
 mamba install -n pimenta -y mafft=7.471 openpyxl pandas
-mamba install -n pimenta -y taxopy krona
+mamba install -n pimenta -y -c bioconda taxopy krona
 conda activate pimenta
 ktUpdateTaxonomy.sh
 
